e1d4a5ea4ad2d5ba139f4931b73542e5c4f9478b
zwr <aerbeisipikaqiu@gmail.com>
2026-08-11 22:43:17 +0800

    ablation_test

--- ablation_test/new_ablation_test3.py
+++ ablation_test/new_ablation_test3.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
+from pathlib import Path
 from torch.utils.data import Dataset, DataLoader
 
 ## =================================================================
@@ -283,10 +284,13 @@ def main():
     EPOCHS = 100            
     LEARNING_RATE = 0.001
     
-    csv_dir_path = r'D:\SAR_database\python2'   
-    patch_dir_path = r'D:\SAR_database\python'  
-    supply_dir_path = r'D:\SAR_database\supply_data' 
-    model_save_path = r'D:\SAR_database\python2\best_yolo_fixed_transect_model.pth' 
+    ROOT = Path(__file__).parent
+    csv_dir_path = ROOT / "python2"
+    patch_dir_path = ROOT / "python"
+
+    supply_dir_path = ROOT / "supply_data"
+
+    model_save_path = ROOT / "best_yolo_fixed_transect_model.pth"
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"⚡ Computing device locking: 【{device}】")
